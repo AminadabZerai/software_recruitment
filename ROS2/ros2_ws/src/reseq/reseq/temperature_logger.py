145502b36543eec555da303c29fbc528cc7696ee
AminadabZerai <aminadabzeray@gmail.com>
2025-10-03 22:58:02 +0200

    Implemented the temperature logger node which subscribes to the /temperature topic and logs the data on the terminal and on a log file when the temperature exceeds 50.

--- ROS2/ros2_ws/src/reseq/reseq/temperature_logger.py
+++ ROS2/ros2_ws/src/reseq/reseq/temperature_logger.py
@@ -1,9 +1,25 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float32
+import datetime as dt
 
 # Define the TemperatureLogger node
+class TemperatureLogger(Node):
+    def __init__(self, log_file):
+        super().__init__('temperature_logger')
+        self.subscription = self.create_subscription(
+            Float32,
+            '/temperature',
+            self.listener_callback,
+            10)
+        self.log_file = log_file
 
+    def listener_callback(self, msg):
+        current_time = dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        if msg.data > 50.0:
+            with open(self.log_file, 'a') as f:
+                f.write(f'{current_time}    Temperature: {msg.data:.2f}°C\n')
+            self.get_logger().warn('Logged temperature: %.2f' % msg.data)
 
 def main(args=None):
     rclpy.init(args=args)
